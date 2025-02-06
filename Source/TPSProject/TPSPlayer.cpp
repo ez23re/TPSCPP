7a1755fc90eb52c1796199530d948b8f17882eb5
ez23re <ez23re@gmail.com>
2025-02-06 15:43:39 +0900

    스나이퍼건모드 UI 완료

--- Source/TPSProject/TPSPlayer.cpp
+++ Source/TPSProject/TPSPlayer.cpp
@@ -6,6 +6,9 @@
 #include "Engine/LocalPlayer.h"
 #include "EnhancedInputSubsystems.h"
 #include "Components/SkeletalMeshComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Blueprint/UserWidget.h"
+#include "Kismet/GameplayStatics.h"
 
 ATPSPlayer::ATPSPlayer()
 {
@@ -56,6 +59,22 @@ ATPSPlayer::ATPSPlayer()
 		GunMeshComp->SetRelativeLocation( FVector( 0 , 60 , 120 ) );
 	}
 
+	// 스나이퍼건 컴포넌트 등록
+	SniperGunMesh = CreateDefaultSubobject<UStaticMeshComponent>( TEXT("SniperGunMesh" ));
+	SniperGunMesh->SetupAttachment( GetMesh() );
+	ConstructorHelpers::FObjectFinder<UStaticMesh> TempSniperMesh ( TEXT( "/Script/Engine.StaticMesh'/Game/SniperGun/sniper11.sniper11'" ) );
+
+	if (TempSniperMesh.Succeeded()) {
+		// 성공시 스태틱 메시 데이터 할당
+		SniperGunMesh->SetStaticMesh( TempSniperMesh.Object );
+
+		// 총 위치 설정
+		SniperGunMesh->SetRelativeLocation( FVector( 0 , 50 , 120 ));
+
+		// 총 크기 설정
+		SniperGunMesh->SetRelativeScale3D( FVector( 0.15f ) );
+
+	}
 }
 
 void ATPSPlayer::BeginPlay()
@@ -71,7 +90,20 @@ void ATPSPlayer::BeginPlay()
 		}
 	}
 
-	JumpMaxCount = 5;
+	// JumpMaxCount = 5;
+	
+	// 스나이퍼 UI 위젯 인스턴스 생성
+	sniperUI = CreateWidget( GetWorld() , SniperUIFactory );
+
+	// 일반 조준 UI 크로스헤어 인스턴스 생성
+	_CrossHairUI = CreateWidget( GetWorld() , CrossHairUIFactory );
+
+	// 일반 조준 UI를 보이도록 등록
+	_CrossHairUI->AddToViewport();
+	
+	// 기본으로 스나이퍼 건을 사용하도록 설정
+	ChangeToSniperGun( FInputActionValue() );
+	
 }
 
 void ATPSPlayer::Tick(float DeltaTime)
@@ -96,18 +128,116 @@ void ATPSPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent
 	
 	auto PlayerInput = Cast<UEnhancedInputComponent>( PlayerInputComponent );
 	if (PlayerInput) {
+
 		PlayerInput->BindAction( IA_LookUp , ETriggerEvent::Triggered , this , &ATPSPlayer::LookUp );
 		PlayerInput->BindAction( IA_Turn , ETriggerEvent::Triggered , this , &ATPSPlayer::Turn );
+
 		PlayerInput->BindAction( IA_PlayerMove , ETriggerEvent::Triggered , this , &ATPSPlayer::Move );
 		PlayerInput->BindAction( IA_Jump , ETriggerEvent::Started , this , &ATPSPlayer::InputJump );
 		PlayerInput->BindAction( IA_Fire , ETriggerEvent::Started , this , &ATPSPlayer::InputFire );
+
+		PlayerInput->BindAction( IA_GrenadeGun , ETriggerEvent::Started , this , &ATPSPlayer::ChangeToGrenadeGun );
+		PlayerInput->BindAction( IA_SniperGun , ETriggerEvent::Started , this , &ATPSPlayer::ChangeToSniperGun );
+
+		PlayerInput->BindAction( IA_Sniper , ETriggerEvent::Started , this , &ATPSPlayer::SniperAim );
+		PlayerInput->BindAction( IA_Sniper , ETriggerEvent::Completed , this , &ATPSPlayer::SniperAim );
+
 	}
 }
 
 void ATPSPlayer::InputFire( const FInputActionValue& inputValue )
 {
+	// 유탄총 사용시
+	if (bUsingGrenadeGun == true) {
 	FTransform FirePosition = GunMeshComp->GetSocketTransform(TEXT("FirePosition"));
 	GetWorld()->SpawnActor<ABullet>( BulletFactory , FirePosition);
+	} 
+	else { // 스나이퍼 건 사용시
+		// LineTrace 의 시작위치
+		FVector startPos = tpsCamComp->GetComponentLocation();
+
+		// LineTrace 의 종료위치
+		FVector endPos = tpsCamComp->GetComponentLocation() + tpsCamComp->GetForwardVector() * 5000.f;
+
+		// LineTrace 의 충돌정보 담을 변수
+		FHitResult hitInfo;
+
+		// 충돌 옵션 설정 변수
+		FCollisionQueryParams params;
+
+		// 자기 자신은 총돌에서 제외
+		params.AddIgnoredActor( this );
+
+		// Channel filter를 이용한 LineTrace 충돌검출
+		bool bHit = GetWorld()->LineTraceSingleByChannel( hitInfo , startPos , endPos, ECollisionChannel::ECC_Visibility, params);
+
+		// LineTrace가 충돌했을 때 
+		if (bHit == true) {
+			// 충돌처리 -> 충돌효과 표현
+			// 총알 파편 효과 트랜스폼 
+			FTransform BulletTrans;
+
+			// 부딪힌 위치를 할당
+			BulletTrans.SetLocation( hitInfo.ImpactPoint );
+
+			// 총알 파편 효과 인스턴스 생성
+			UGameplayStatics::SpawnEmitterAtLocation( GetWorld() , BulletEffectFactory , BulletTrans );
+
+			auto hitComp = hitInfo.GetComponent();
+
+			// 만약에 컴포넌트에 물리가 적용되어 있다면
+			if (hitComp&&hitComp->IsSimulatingPhysics()) {
+				// 조준한 방향이 필요
+				FVector dir = (endPos - startPos).GetSafeNormal();
+
+				// 날려 버릴 힘 ( F = ma )
+				FVector force = dir * hitComp->GetMass() * 500000;
+
+				// 그 방향으로 날려버리고 싶다
+				hitComp->AddForceAtLocation( force , hitInfo.ImpactPoint );
+
+			}
+		}
+	}
+}
+
+void ATPSPlayer::ChangeToGrenadeGun( const FInputActionValue& inputValue )
+{
+	// 유탄총 사용중으로 체크
+	bUsingGrenadeGun = true;
+	SniperGunMesh->SetVisibility( !bUsingGrenadeGun );
+	GunMeshComp->SetVisibility( bUsingGrenadeGun );
+}
+
+void ATPSPlayer::ChangeToSniperGun( const FInputActionValue& inputValue )
+{
+	bUsingGrenadeGun = false;
+	SniperGunMesh->SetVisibility( true );
+	GunMeshComp->SetVisibility( false );
+}
+
+void ATPSPlayer::SniperAim( const FInputActionValue& inputValue )
+{
+	// 스나이퍼건 모드가 아니라면 처리하지 않는다
+	if (bUsingGrenadeGun) {
+		return;
+	}
+	
+	// Pressed 입력 처리 : Sniper 조준 모드 활성화
+	if (bSniperAim == false) {
+		bSniperAim = true; 
+		sniperUI->AddToViewport();
+		tpsCamComp->SetFieldOfView( 45 ); // 카메라 시야각 FOV 45도로
+		// 일반 조준 UI를 제거
+		_CrossHairUI->RemoveFromParent();
+	}
+	else {
+		bSniperAim = false; // Released 입력 처리 :  Sniper 조준 모드 비활성화
+		sniperUI->RemoveFromParent();
+		tpsCamComp->SetFieldOfView(90); // 카메라 시야각 복원
+		// 일반 조준 UI 등록
+		_CrossHairUI->AddToViewport();
+	}
 }
 
 void ATPSPlayer::Turn( const FInputActionValue& inputValue )
--- Source/TPSProject/TPSPlayer.h
+++ Source/TPSProject/TPSPlayer.h
@@ -4,6 +4,7 @@
 #include "GameFramework/Character.h"
 #include "InputActionValue.h"
 #include "Bullet.h"
+#include "Blueprint/UserWidget.h"
 #include "TPSPlayer.generated.h"
 
 UCLASS()
@@ -40,8 +41,48 @@ public:
 	// 총알 발사 처리함수
 	void InputFire(const FInputActionValue& inputValue);
 
-	UPROPERTY( EditDefaultsOnly, Category=Camera)
+	// 스나이퍼건 스태틱메시 추가
+	UPROPERTY(VisibleAnywhere, Category=GunMesh)
+	class UStaticMeshComponent* SniperGunMesh;
+
+	// 유탄총 사용중인지 여부
+	bool bUsingGrenadeGun = true;
+
+	// 유탄총으로 변경
+	void ChangeToGrenadeGun( const FInputActionValue& inputValue );
+
+	// 스나이퍼건으로 변경
+	void ChangeToSniperGun( const FInputActionValue& inputValue );
+
+	UPROPERTY( VisibleAnywhere , BlueprintReadOnly , Category = Camera )
 	class UCameraComponent* tpsCamComp;
+
+	// 스나이퍼모드
+	UPROPERTY(EditDefaultsOnly, Category=Input)
+	class UInputAction* IA_Sniper;
+
+	void SniperAim( const FInputActionValue& inputValue );
+	bool bSniperAim = false;
+
+	// 스나이퍼 UI 위젯 공장
+	UPROPERTY(EditDefaultsOnly, Category=SniperUI)
+	TSubclassOf<class UUserWidget> SniperUIFactory; // 헤더추가 해줘야됨?
+
+	// 스나이퍼 UI Widget 인스턴스
+	UPROPERTY()
+	class UUserWidget* sniperUI;
+
+	// 총알 파편 효과 공장
+	UPROPERTY(EditAnywhere, Category=BulletEffect)
+	class UParticleSystem* BulletEffectFactory;
+
+	// 일반 조준 크로스헤어UI 위젯
+	UPROPERTY(EditDefaultsOnly, Category=SniperUI)
+	TSubclassOf<class UUserWidget> CrossHairUIFactory;
+
+	// 크로스헤어 인스턴스
+	class UUserWidget* _CrossHairUI;
+
 
 	// Input
 	UPROPERTY(EditDefaultsOnly, Category=Input)
@@ -55,6 +96,12 @@ public:
 
 	UPROPERTY(EditDefaultsOnly, Category=Input)
 	class UInputAction* IA_PlayerMove; // 사용자 좌우 이동
+
+	UPROPERTY(EditDefaultsOnly, Category=Input)
+	class UInputAction* IA_GrenadeGun;
+
+	UPROPERTY(EditDefaultsOnly, Category=Input)
+	class UInputAction* IA_SniperGun;
 
 	UPROPERTY(EditDefaultsOnly, Category=Input)
 
