6b874dd19c3f5f21be209f0d079657db8774681b
ez23re <ez23re@gmail.com>
2025-02-11 13:56:52 +0900

    250211: 알파시작

---
+++ Source/TPSProject/TPSPlayerAnimInstance.cpp
@@ -0,0 +1,16 @@
+﻿#include "TPSPlayerAnimInstance.h"
+#include "TPSPlayer.h"
+
+void UTPSPlayerAnimInstance::NativeUpdateAnimation( float DeltaSeconds )
+{
+	Super::NativeUpdateAnimation( DeltaSeconds );
+
+	ATPSPlayer* player = Cast<ATPSPlayer>( TryGetPawnOwner() );
+	if (player == nullptr) {
+		return;
+	}
+
+	FVector velocity = player->GetVelocity();
+	FVector forwardvector = player->GetActorForwardVector();
+	Speed = FVector::DotProduct( velocity , forwardvector );
+}
